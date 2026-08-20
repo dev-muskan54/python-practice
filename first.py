fbbe75c59c12602848c8c47de0c9908ef0a74fdb
Ziya Muskan <muskanvlogs594@gmail.com>
2026-08-20 21:53:30 +0530

    Add first Python program

---
+++ first.py
@@ -0,0 +1,7 @@
+name = "Ziya Muskan"
+
+print("Hello, my name is", name)
+print("I am a BCA student.")
+print("I am interested in Artificial Intelligence and Machine Learning.")
+print("I am learning Python and other programming skills.")
+print("My goal is to build a successful career in AI and technology.")
